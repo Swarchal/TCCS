1f65296f5146f48d56edc1a8a0aaf49108e8c6a6
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-06-09 15:16:51 +0100

    fix failing test, missing bracket

--- tests/testthat/test-theta0.R
+++ tests/testthat/test-theta0.R
@@ -6,3 +6,5 @@ test_that("theta0 returns expected values",{
 	expect_equal(theta0(c(-1, 0)), 180, tolerance = 1e-3)
 	expect_equal(theta0(c(0, -1)), 270, tolerance = 1e-3)
 	expect_error(theta0(c(0, 1, 1)))
+
+}
