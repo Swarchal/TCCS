22187ae93e28ec830c8c35019337a7c4d7101fe9
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-08-17 14:43:41 +0100

    finish cossim_to_angsim_check

--- python/conversions.py
+++ python/conversions.py
@@ -1,7 +1,12 @@
+from math import acos, pi
+
+
 def cossim_to_angsim(x):
     """ converts cosine similarity to angular similarity """
-    pass
+    return 1 - acos(x) / pi
 
 
 def cossim_to_angsim_check(x):
-    """
+    """ check input for cossim_to_angsim """
+    if min(x) < -1 or max(x) > 1:
+        raise ValueError("input values are outside expected range of -1 to 1")
